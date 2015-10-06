e7de333806fb743bead21373a419d845af96e690
dwschrashun <dwschrashun@gmail.com>
2015-10-06 11:05:42 -0400

    added comment

--- chromeExt/contentScripts/spotify.js
+++ chromeExt/contentScripts/spotify.js
@@ -17,6 +17,7 @@ function onPlayerChange() {
 	});
 }
 
+//timeout set in order to catch changes to artist name on DOM, which is fired after track name change
 function getSongInfo() {
 	var songTitle = $($("#app-player").contents().find("#track-name")[0]).text();
 	var duration = $($("#app-player").contents().find("#track-length")).text();
