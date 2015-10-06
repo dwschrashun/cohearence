b43c56aef9e8ca118f3977f57ba68b96efdb9dd3
dwschrashun <dwschrashun@gmail.com>
2015-10-06 11:04:32 -0400

    spotify scrobble now catches correct artist

--- chromeExt/contentScripts/spotify.js
+++ chromeExt/contentScripts/spotify.js
@@ -1,3 +1,5 @@
+//watching track name text on right side player for change
+
 function onPlayerChange() {
 	var player = $($("#app-player").contents().find("#track-name")[0]);
 	console.log('got the player', player);
@@ -5,30 +7,40 @@ function onPlayerChange() {
 		counter++;
 		if (counter == 2) {
 			// console.log('song changed');
-			var song = getSongInfo();
-			console.log('song', song);
-			counter = 0;
+			var song;
+			getSongInfo().then(function (songObj) {
+				song = songObj;
+				console.log('song', song);
+				counter = 0;
+			});
 		}
 	});
 }
 
 function getSongInfo() {
 	var songTitle = $($("#app-player").contents().find("#track-name")[0]).text();
-	var artist = $($("#app-player").contents().find("#track-artist > a")[0]).text();
 	var duration = $($("#app-player").contents().find("#track-length")).text();
 	var href = $($("#app-player").contents().find("#track-name > a")[0]).attr("href");
-	var videoTitle = artist + ' - ' + songTitle;
-	console.log('artist', artist);
-	var songObj = {
-	    message: "spotifySng",
-	    href: href,
-	    videoTitle: videoTitle,
-	    category: 'Music',
-	    duration: duration,
-	    songTitle: songTitle,
-	    artist: artist
-	};
-	return songObj;
+	var song = new Promise (function (resolve, reject) {
+		return resolve (
+			setTimeout(function () {
+				artist = $($("#app-player").contents().find("#track-artist > a")[0]).text();
+				var videoTitle = artist + ' - ' + songTitle;
+				var songObj = {
+				    message: "spotifySng",
+				    href: href,
+				    videoTitle: videoTitle,
+				    category: 'Music',
+				    duration: duration,
+				    songTitle: songTitle,
+				    artist: artist
+				};
+				console.log('spotify songObj', songObj);
+				return songObj;
+			}, 500)
+		);
+	});
+	return song;
 }
 
 var counter = 0;
