65e83a835ecb2ce226fe17534a26133ef7a47548
Angelo <angelo@Angelos-MacBook-Pro.local>
2015-10-06 19:53:14 -0400

    showing iframe sometimes

--- chromeExt/popup/player/player.js
+++ chromeExt/popup/player/player.js
@@ -11,7 +11,55 @@ app.config(function($stateProvider) {
 	})
 })
 
+// 2. This code loads the IFrame Player API code asynchronously.
+			var tag = document.createElement('script');
+
+			tag.src = "https://www.youtube.com/iframe_api";
+			var firstScriptTag = document.getElementsByTagName('script')[0];
+			firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+			// 3. This function creates an <iframe> (and YouTube player)
+			//    after the API code downloads.
+			var player;
+			function onYouTubeIframeAPIReady() {
+				console.log('onYouTubeIframeAPIReady run');
+				player = new YT.Player('player', {
+					height: '390',
+					width: '640',
+					videoId: 'u3Y_sbAHdPw',
+					events: {
+						'onReady': onPlayerReady,
+						'onStateChange': onPlayerStateChange
+					}
+				});
+			}
+			console.log('document ====== ',document);
+
+			// 4. The API will call this function when the video player is ready.
+			function onPlayerReady(event) {
+				event.target.playVideo();
+			}
+
+			// 5. The API calls this function when the player's state changes.
+			//    The function indicates that when playing a video (state=1),
+			//    the player should play for six seconds and then stop.
+			var done = false;
+			function onPlayerStateChange(event) {
+				if (event.data == YT.PlayerState.PLAYING && !done) {
+					setTimeout(stopVideo, 6000);
+					done = true;
+				}
+			}
+			function stopVideo() {
+				player.stopVideo();
+			}
+
 app.controller('playerCtrl', function($scope, LoginFactory, theUser, $state) {
+
+
+
+
+
 	console.log(theUser);
 	$scope.musicLibrary = theUser.musicLibrary;
 	$scope.logout = function() {
