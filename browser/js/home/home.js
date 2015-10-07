2f4792e0655cd3732558bcd7002201bcfad473b7
varunjayaraman <varun.jayaraman@gmail.com>
2015-10-07 13:07:53 -0400

    added landing page and styled login and signup

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,6 +1,16 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'js/home/home.html'
+        templateUrl: 'js/home/home.html',
+        controller: 'HomeController',
+        resolve: {
+        	theUser: function(AuthService) {
+        		return AuthService.getLoggedInUser()
+        	}
+        }
     });
 });
+// console.log(app);
+app.controller('HomeController', function($scope, AuthService, $state, theUser) {
+	if(!theUser) $state.go('landing');
+})
---
+++ browser/js/signup/signup.js
@@ -0,0 +1,27 @@
+app.config(function ($stateProvider) {
+
+   $stateProvider.state('signup', {
+       url: '/signup',
+       templateUrl: 'js/signup/signup.html',
+       controller: 'SignupCtrl'
+   });
+
+});
+
+app.controller('SignupCtrl', function ($scope, AuthService, $state) {
+
+   $scope.error = null;
+
+   $scope.sendSignup= function (signupInfo) {
+       $scope.error = null;
+       AuthService.signup(signupInfo)
+       .then(function () {
+           $state.go('home');
+       })
+       .catch(function () {
+           $scope.error = 'Email is taken!';
+       });
+
+   };
+
+});
