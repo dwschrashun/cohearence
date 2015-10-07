4611b086fa61a213dd6fe45aee30ec58ce2514de
dwschrashun <dwschrashun@gmail.com>
2015-10-07 18:16:12 -0400

    somewhat working w messages sent to player on background

--- chromeExt/background/router.js
+++ chromeExt/background/router.js
@@ -1,3 +1,5 @@
+var player;
+
 window.onload = function () {
     chrome.storage.sync.get("user", function (user) {
         console.log('user', user);
@@ -18,9 +20,31 @@ window.onload = function () {
         	console.log('user not logged in probably should do something about that');
         }
     });
+
+    console.log("making player");
+    var backgroundDoc = chrome.extension.getBackgroundPage().document;
+    console.log("backgroundDoc:", backgroundDoc);
+    var tag = backgroundDoc.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = backgroundDoc.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    player = backgroundDoc.getElementById("player");
+    setTimeout(() => {
+      player = new YT.Player(player, {
+        height: '390',
+        width: '640',
+        videoId: 'H_IcrHMbb8M',
+        events: {
+            // "onReady": onPlayerReady,
+            // "onStateChange": onPlayerStateChange
+        }
+      });
+      console.log("player on first open:", player);
+    }, 2000);
 };
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    console.log("REQUEST received:", request);
     if (request.message === 'YouTube') {
         sendSong(request);
         sendResponse({
@@ -39,6 +63,31 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
             response: "hey we got your song at the router"
         });
     }
+
+    if (request.message === "play") {
+        console.log("play received", player);
+        player.playVideo();
+    }
+
+    if (request.message === "pause") {
+        player.pauseVideo();
+    }
+
+    if (request.message === "cue") {
+        console.log("cue received", player);
+        player.cueVideoById(request.id);
+    }
+
+    if (request.message === "unMute") {
+        console.log("unMute received", player);
+        player.unMute();
+    }
+
+    if (request.message === "test") {
+        console.log("test received", player);
+        console.log(player.getCurrentTime());
+    }
+
     return true;
 });
 
@@ -67,35 +116,56 @@ function sendSong(songObj) {
 	});
 }
 
+var counter = 0;
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo && changeInfo.status == "complete") {
+        if (tab.url.indexOf('https://www.youtube.com/watch') !== -1) {
+            counter++;
+            console.log('url changed', counter);
+            if (counter == 2) {
+                console.log("Tab updated: ", tab.url, ' sending message', counter, 'tabId', tabId);
+                chrome.tabs.sendMessage(tabId, {
+                    message: "newSongLoaded"
+                }, {}, function (response) {
+                    console.log("response in newSongLoaded emitter", response);
+                });
+                counter = 0;
+            }
+        }
+    }
+});
+
+
 // chrome.webNavigation.onCompleted.addListener(function (details) {
 //     console.log("On completed", details);
 // });
 
-var previousUrl = true;
+// var previousUrl = true;
 
-chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
-    // console.log("state updated: previousUrl, new url:", previousUrl, details.url);
-    if (previousUrl) {
-        //  	chrome.tabs.query({currentWindow: true, active: true}, function(tabArray) {
-        // 		var port = chrome.tabs.connect(tabArray[0].id, {name: "ytConnect"});
-        //  		console.log("sending");
-        // 		port.postMessage({message: "newSongLoaded"});
-        // 		});
-        chrome.tabs.query({
-            active: true,
-            currentWindow: true
-        }, function (tabs) {
-            console.log("in tabs", tabs);
-			console.log('tab 0: ',tabs[0]);
-            chrome.tabs.sendMessage(tabs[0].id, {
-                message: "newSongLoaded"
-            }, {}, function (response) {
-				console.log('response:', response);
-                previousUrl = undefined;
-                //console.log("response in newSongLoaded emitter", response);
-            });
-        });
-    } else {
-        previousUrl = details.url;
-    }
-});
+// chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
+//     // console.log("state updated: previousUrl, new url:", previousUrl, details.url);
+//     if (previousUrl) {
+//         //  	chrome.tabs.query({currentWindow: true, active: true}, function(tabArray) {
+//         // 		var port = chrome.tabs.connect(tabArray[0].id, {name: "ytConnect"});
+//         //  		console.log("sending");
+//         // 		port.postMessage({message: "newSongLoaded"});
+//         // 		});
+//         chrome.tabs.query({
+//             active: true,
+//             currentWindow: true
+//         }, function (tabs) {
+//             console.log("in tabs", tabs);
+// 			console.log('tab 0: ',tabs[0]);
+//             chrome.tabs.sendMessage(tabs[0].id, {
+//                 message: "newSongLoaded"
+//             }, {}, function (response) {
+// 				console.log('response:', response);
+//                 previousUrl = undefined;
+//                 //console.log("response in newSongLoaded emitter", response);
+//             });
+//         });
+//     } else {
+//         previousUrl = details.url;
+//     }
+// });
--- chromeExt/popup/player/player.js
+++ chromeExt/popup/player/player.js
@@ -8,65 +8,117 @@ app.config(function($stateProvider) {
 				return LoginFactory.isLoggedIn();
 			}
 		}
-	})
-})
-
-
-
-
+	});
+});
 
 app.controller('playerCtrl', function($scope, LoginFactory, PlayerFactory, theUser, $state) {
 
-	var window = chrome.extension.getBackgroundPage();
-	console.log('window document',window.document);
-	var tag = window.document.createElement('script');
-	tag.src = "https://www.youtube.com/iframe_api";
-	var firstScriptTag = document.getElementsByTagName('script')[0];
-	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-	var player;
-	setTimeout(() => {
-	  player = new YT.Player('player', {
-	    height: '390',
-	    width: '640',
-	    videoId: 'H_IcrHMbb8M',
-	  });
-	},500)
+	// var backgroundDoc = chrome.extension.getBackgroundPage().document;
+	// console.log("backgroundDoc:", backgroundDoc);
+	// var tag = backgroundDoc.createElement('script');
+	// tag.src = "https://www.youtube.com/iframe_api";
+	// var firstScriptTag = backgroundDoc.getElementsByTagName('script')[0];
+	// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+	// var player;
+	// if (!backgroundDoc.getElementsByTagName('iframe').length) {
+	// 	console.log("making player");
+	// 	player = backgroundDoc.getElementById("player");
+	// 	setTimeout(() => {
+	// 		if (player)
+	// 	  player = new YT.Player(player, {
+	// 	    height: '390',
+	// 	    width: '640',
+	// 	    videoId: 'H_IcrHMbb8M',
+	// 	    events: {
+	// 	    	"onReady": onPlayerReady,
+	// 	    	"onStateChange": onPlayerStateChange
+	// 	    }
+	// 	  });
+	// 	  console.log("player on first open:", player);
+	// 	}, 2000);
+	// }
+	// else {
+	// 	player = backgroundDoc.getElementsByTagName('iframe')[0];
+	// 	console.log("player on reopen:", player);
+	// }
 
-$scope.loadSong = function(song){
-	$scope.currentSong = song;
-	if(song.source.domain === 'YouTube'){
-		player.cueVideoById(PlayerFactory.getVideoId(song));
-		player.playVideo();
+	function onPlayerReady(event) {
+		console.log("player ready", event.target);
+		console.log("player", player);
+		event.target.unMute();
+        event.target.playVideo();
+    }
+
+    function onPlayerStateChange () {
+    	console.log("player state changed");
+    }
+	
+	$scope.loadSong = function(song){
+		$scope.currentSong = song;
+		if (song.source.domain === 'YouTube') {
+			var request = {message: "cue"};
+			request.id = PlayerFactory.getVideoId(song);
+			console.log("cue message sending", request);
+			chrome.runtime.sendMessage(request, function (response) {
+	        	console.log('response from router:', response);
+	    	});
+
+			// var id = PlayerFactory.getVideoId(song);
+			// player.cueVideoById(id);
+			// console.log("player w new song", player);
+			// player.unMute();
+			// player.playVideo();
+		}
+	};
+
+	$scope.playVideo = function(){
+		var request = {message: "play"};
+		console.log("play message sending", request);
+		chrome.runtime.sendMessage(request, function (response) {
+	        console.log('response from router:', response);
+	    });
+		// player.playVideo();
 	}
-}
 
-$scope.playVideo = function(){
-	player.playVideo();
-}
+	$scope.pauseVideo = function(){
+		var request = {message: "pause"};
+		chrome.runtime.sendMessage(request, function (response) {
+	        console.log('response from router:', response);
+	    });
+		// player.pauseVideo();
+	}
 
-$scope.pauseVideo = function(){
-	player.pauseVideo();
-}
+	$scope.skipForward = function(){
+		var request = {message: "test"};
+		chrome.runtime.sendMessage(request, function (response) {
+	        console.log('response from router:', response);
+	    });
+		player.seekTo(player.getCurrentTime() + 15);
+	}
 
-$scope.skipForward = function(){
-	player.seekTo(player.getCurrentTime() + 15);
-}
+	$scope.unMute = function () {
+		var request = {message: "unMute"};
+		chrome.runtime.sendMessage(request, function (response) {
+	        console.log('response from router:', response);
+	    });
+	}
 
-$scope.startOrStopFastForward = function(toggle) {
-	console.log('perform on fast forward ', toggle);
-	var ff = () => player.seekTo(player.getCurrentTime() + 1);
-	if (toggle === 'stop') clearInterval(fastForward);
-	else fastForward = setInterval(ff, 100);
-}
 
-$scope.startOrStopRewind = function(toggle) {
-	console.log('perform on fast forward ', toggle);
-	var ff = () => player.seekTo(player.getCurrentTime() - 1);
-	if (toggle === 'stop') clearInterval(fastForward);
-	else fastForward = setInterval(ff, 100);
-}
+	$scope.startOrStopFastForward = function(toggle) {
+		console.log('perform on fast forward ', toggle);
+		var ff = () => player.seekTo(player.getCurrentTime() + 1);
+		if (toggle === 'stop') clearInterval(fastForward);
+		else fastForward = setInterval(ff, 100);
+	}
 
-	console.log(theUser);
+	$scope.startOrStopRewind = function(toggle) {
+		console.log('perform on fast forward ', toggle);
+		var ff = () => player.seekTo(player.getCurrentTime() - 1);
+		if (toggle === 'stop') clearInterval(fastForward);
+		else fastForward = setInterval(ff, 100);
+	}
+
+	console.log("theUser:", theUser);
 	$scope.musicLibrary = theUser.musicLibrary;
 	$scope.logout = function() {
 		LoginFactory.logout()
