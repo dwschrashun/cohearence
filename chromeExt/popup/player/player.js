2c2a6bd96a39e3886daba9c4c0ea12c8a9fd8655
Ryan Dwyer <ryankdwyer@gmail.com>
2015-10-07 20:57:14 -0400

    Soundcloud working on the background page

--- chromeExt/popup/player/player.js
+++ chromeExt/popup/player/player.js
@@ -12,49 +12,11 @@ app.config(function($stateProvider) {
 });
 
 app.controller('playerCtrl', function($scope, LoginFactory, PlayerFactory, theUser, $state) {
+	$scope.currentService = 'Soundcloud';
 
-	// var backgroundDoc = chrome.extension.getBackgroundPage().document;
-	// console.log("backgroundDoc:", backgroundDoc);
-	// var tag = backgroundDoc.createElement('script');
-	// tag.src = "https://www.youtube.com/iframe_api";
-	// var firstScriptTag = backgroundDoc.getElementsByTagName('script')[0];
-	// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-	// var player;
-	// if (!backgroundDoc.getElementsByTagName('iframe').length) {
-	// 	console.log("making player");
-	// 	player = backgroundDoc.getElementById("player");
-	// 	setTimeout(() => {
-	// 		if (player)
-	// 	  player = new YT.Player(player, {
-	// 	    height: '390',
-	// 	    width: '640',
-	// 	    videoId: 'H_IcrHMbb8M',
-	// 	    events: {
-	// 	    	"onReady": onPlayerReady,
-	// 	    	"onStateChange": onPlayerStateChange
-	// 	    }
-	// 	  });
-	// 	  console.log("player on first open:", player);
-	// 	}, 2000);
-	// }
-	// else {
-	// 	player = backgroundDoc.getElementsByTagName('iframe')[0];
-	// 	console.log("player on reopen:", player);
-	// }
-
-	function onPlayerReady(event) {
-		console.log("player ready", event.target);
-		console.log("player", player);
-		event.target.unMute();
-        event.target.playVideo();
-    }
-
-    function onPlayerStateChange () {
-    	console.log("player state changed");
-    }
-	
 	$scope.loadSong = function(song){
 		$scope.currentSong = song;
+		$scope.currentService = song.source.domain;
 		if (song.source.domain === 'YouTube') {
 			var request = {message: "cue"};
 			request.id = PlayerFactory.getVideoId(song);
@@ -72,51 +34,48 @@ app.controller('playerCtrl', function($scope, LoginFactory, PlayerFactory, theUs
 	};
 
 	$scope.playVideo = function(){
-		var request = {message: "play"};
+		var request = {message: "playerAction", action: "play", service: $scope.currentService};
 		console.log("play message sending", request);
 		chrome.runtime.sendMessage(request, function (response) {
 	        console.log('response from router:', response);
 	    });
-		// player.playVideo();
-	}
+	};
 
 	$scope.pauseVideo = function(){
-		var request = {message: "pause", service: "YouTube"};
+		var request = {message: "playerAction", action: "pause", service: $scope.currentService};
 		chrome.runtime.sendMessage(request, function (response) {
 	        console.log('response from router:', response);
 	    });
-		// player.pauseVideo();
-	}
+	};
 
 	$scope.skipForward = function(){
-		var request = {message: "test", service: "YouTube"};
+		var request = {message: "test", service: $scope.currentService};
 		chrome.runtime.sendMessage(request, function (response) {
 	        console.log('response from router:', response);
 	    });
 		player.seekTo(player.getCurrentTime() + 15);
-	}
+	};
 
 	$scope.unMute = function () {
-		var request = {message: "unMute", service: "YouTube"};
+		var request = {message: "unMute", service: $scope.currentService};
 		chrome.runtime.sendMessage(request, function (response) {
 	        console.log('response from router:', response);
 	    });
-	}
-
+	};
 
 	$scope.startOrStopFastForward = function(toggle) {
 		console.log('perform on fast forward ', toggle);
 		var ff = () => player.seekTo(player.getCurrentTime() + 1);
 		if (toggle === 'stop') clearInterval(fastForward);
 		else fastForward = setInterval(ff, 100);
-	}
+	};
 
 	$scope.startOrStopRewind = function(toggle) {
 		console.log('perform on fast forward ', toggle);
 		var ff = () => player.seekTo(player.getCurrentTime() - 1);
 		if (toggle === 'stop') clearInterval(fastForward);
 		else fastForward = setInterval(ff, 100);
-	}
+	};
 
 	console.log("theUser:", theUser);
 	$scope.musicLibrary = theUser.musicLibrary;
@@ -140,10 +99,10 @@ app.controller('playerCtrl', function($scope, LoginFactory, PlayerFactory, theUs
 	$scope.pause = function() {
 		var cl = document.getElementsByTagName("iframe")[0];
 		cl.src = "";
-	}
+	};
 
 	$scope.play = function() {
 		var cl = document.getElementsByTagName("iframe")[0];
 		cl.src = "http://bandcamp.com/EmbeddedPlayer/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/track=" + $scope.id + "/transparent=true/";
-	}
+	};
 });
