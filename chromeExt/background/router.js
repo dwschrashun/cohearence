e3434eb6c02accce846ffb03c66ddd8d9920320d
varunjayaraman <varun.jayaraman@gmail.com>
2015-10-09 11:36:47 -0400

    got local storage to work

--- chromeExt/background/router.js
+++ chromeExt/background/router.js
@@ -4,30 +4,47 @@ var soundcloudVideo;
 var bandcampVideo;
 var serviceMethods = {};
 
+console.log('localstorage!', localStorage);
+
+function getUser() {
+  return JSON.parse(localStorage.getItem("cohearenceUser"));
+}
+
+function setUser(library) {
+  var theUser = getUser();
+  theUser.musicLibrary = library;
+  var stringifiedUpdatedUser = JSON.stringify(theUser);
+  localStorage.setItem("cohearenceUser", stringifiedUpdatedUser);
+}
+
+function getBackendUserAndUpdateLocalStorage() {
+  var user = getUser();
+  console.log('onload', user);
+  if (user) {
+    $.ajax({
+      url: 'http://localhost:1337/api/users/' + user._id + '/library',
+      method: 'GET',
+      dataType: "json"
+    })
+    .done(function (response) {
+      setUser(response);
+    })
+    .fail(function (error) {
+      console.error(error);
+    });
+  } else {
+    console.log('user not logged in probably should do something about that');
+  }
+}
+
 window.onload = function () {
   backgroundDoc = $(chrome.extension.getBackgroundPage().document);
   soundcloudVideo = backgroundDoc.find('#soundcloudPlayer');
   bandcampVideo = backgroundDoc.find('#bandcampPlayer');
   createYouTubeVideo();
-
-  chrome.storage.sync.get("user", function (user) {
-    if (user.user) {
-      $.ajax({
-        url: 'http://localhost:1337/api/users/' + user.user._id + '/library',
-        method: 'GET',
-        dataType: "json"
-      }).done(function (response) {
-        chrome.storage.sync.set({
-            user: response
-        }, function () {});
-      }).fail(function (error) {
-        console.error(error);
-      });
-    } else {
-      console.log('user not logged in probably should do something about that');
-    }
-  });
-};
+  //get user from backend and update in local storage if exists
+  getBackendUserAndUpdateLocalStorage();
+}
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === 'scrobble') {
@@ -51,23 +68,21 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 });
 
 function sendSong(songObj) {
-	chrome.storage.sync.get('user', function (user) {
-		//console.log("user pre-library put:", user);
-	    if (!user && !user.user && !user.user._id) {
-	    	console.log('there is no user logged in');
-	    	return;
-	    }
-	    $.ajax({
-	        url: "http://localhost:1337/api/users/" + user.user._id + "/library",
-	        method: 'PUT',
-	        data: songObj,
-	        dataType: "json"
-	    }).done(function (response) {
-	        user.user.musicLibrary = response;
-	        chrome.storage.sync.set({user: user.user}, function () {
-	        });
-	    }).fail(function (error) {
-	        console.log(error);
-	    });
-	});
+  var user = getUser();
+  console.log('sendsong', user);
+  if (user) {
+    $.ajax({
+      url: "http://localhost:1337/api/users/" + user._id + "/library",
+      method: 'PUT',
+      data: songObj,
+      dataType: "json"
+    })
+    .done(function(response) {
+      console.log('AJAX RESPONSE', response);
+      setUser(response);
+    })
+    .fail(function(error) {
+      console.log(error);
+    })
+  }
 }
