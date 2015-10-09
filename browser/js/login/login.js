3fdc4edfbd3f46d7b056288ad34f2a09fe58ec11
Stephen Colacurcio <stephencolacurcio@gmail.com>
2015-10-09 14:57:34 -0400

    removed login and signup states

--- browser/js/login/login.js
+++ browser/js/login/login.js
@@ -1,12 +1,12 @@
-app.config(function ($stateProvider) {
-
-    $stateProvider.state('login', {
-        url: '/login',
-        templateUrl: 'js/login/login.html',
-        controller: 'LoginCtrl'
-    });
-
-});
+// app.config(function ($stateProvider) {
+//
+//     $stateProvider.state('login', {
+//         url: '/login',
+//         templateUrl: 'js/login/login.html',
+//         controller: 'LoginCtrl'
+//     });
+//
+// });
 
 app.controller('LoginCtrl', function ($scope, AuthService, $state) {
 
