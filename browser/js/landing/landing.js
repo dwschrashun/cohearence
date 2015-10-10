006821c97aa793bdf8e648457b87be41a9f98d4a
Stephen Colacurcio <stephencolacurcio@gmail.com>
2015-10-10 16:31:59 -0400

    fixed login error bug

--- browser/js/landing/landing.js
+++ browser/js/landing/landing.js
@@ -10,9 +10,11 @@ app.controller('LandingController', function($scope) {
 	$scope.toggleLogin = function(){
 		$scope.loggingIn = ! $scope.loggingIn;
 		$scope.signingUp = false;
+		$scope.error = null;
 	};
 	$scope.toggleSignup = function(){
 		$scope.signingUp = ! $scope.signingUp;
 		$scope.loggingIn = false;
+		$scope.error = null;
 	};
 });
--- browser/js/signup/signup.js
+++ browser/js/signup/signup.js
@@ -9,18 +9,17 @@
 // });
 
 app.controller('SignupCtrl', function ($scope, AuthService, $state) {
+	$scope.error = null;
 
-   $scope.error = null;
-
-   $scope.sendSignup= function (signupInfo) {
-       $scope.error = null;
-       AuthService.signup(signupInfo)
-       .then(function () {
-           $state.go('home');
-       })
-       .catch(function () {
-           $scope.error = 'Email is taken!';
-       });
-   };
+	$scope.sendSignup= function (signupInfo) {
+	$scope.error = null;
+	AuthService.signup(signupInfo)
+		.then(function () {
+			$state.go('home');
+		})
+		.catch(function () {
+			$scope.error = 'Email is taken!';
+		});
+	};
 
 });
