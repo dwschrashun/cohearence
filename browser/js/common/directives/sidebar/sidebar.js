8322b2a895436ddcd774bbdaf138cffbf0fb3ee8
Stephen Colacurcio <stephencolacurcio@gmail.com>
2015-10-11 21:45:04 -0400

    sidebar starting to look dope

--- browser/js/common/directives/sidebar/sidebar.js
+++ browser/js/common/directives/sidebar/sidebar.js
@@ -2,8 +2,30 @@ app.directive('sidebar', function() {
 	return {
 		restrict: 'E',
 		templateUrl: 'js/common/directives/sidebar/sidebar.html',
-		link: function(scope, elem, attrs) {
+		controller: function ($scope, $state) {
+			$scope.currentItem = "library";
+			$scope.items = [
+				"Library",
+				"Playlists",
+				"Stats",
+				"Friends",
+				"Settings"
+			];
+			$scope.setCurrentItem = function(item){
+				$scope.currentItem = item;
+				if (item == "Playlists") {
+					$scope.showingPlaylists = !$scope.showingPlaylists;
+					console.log($scope.showingPlaylists);
+				} else {
+					$scope.showingPlaylists = false;
+				}
 
+				if (item == "Library") {
+					console.log('going home');
+					$state.go('home');
+				}
+				console.log($scope.currentItem);
+			};
 		}
-	}
+	};
 });
