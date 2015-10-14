6d682653bd1a6a1892bd663526323c9fe35b2ccd
Ryan Dwyer <ryankdwyer@gmail.com>
2015-10-14 12:22:01 -0400

    login persists on web app - redirects to library

--- browser/js/app.js
+++ browser/js/app.js
@@ -10,12 +10,16 @@ app.config(function ($urlRouterProvider, $locationProvider) {
 
 // This app.run is for controlling access to specific states.
 app.run(function ($rootScope, AuthService, $state) {
-
     // The given state requires an authenticated user.
     var destinationStateRequiresAuth = function (state) {
         return state.data && state.data.authenticate;
     };
-
+    
+    function redirectToLibrary () {
+        var user = AuthService.getLoggedInUser();
+        if (user) $state.go('home');
+    }
+    redirectToLibrary();
     // $stateChangeStart is an event fired
     // whenever the process of changing a state begins.
     $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
--- browser/js/landing/landing.js
+++ browser/js/landing/landing.js
@@ -1,18 +1,25 @@
-app.config(function($stateProvider) {
-	$stateProvider.state('landing', {
-		url: '/',
-		templateUrl: 'js/landing/landing.html',
-		controller: 'LandingController'
-	});
+app.config(function ($stateProvider) {
+    $stateProvider.state('landing', {
+        url: '/',
+        templateUrl: 'js/landing/landing.html',
+        controller: 'LandingController'
+    });
 });
 
-app.controller('LandingController', function($scope) {
-	$scope.toggleLogin = function(){
-		$scope.loggingIn = ! $scope.loggingIn;
-		$scope.signingUp = false;
-	};
-	$scope.toggleSignup = function(){
-		$scope.signingUp = ! $scope.signingUp;
-		$scope.loggingIn = false;
-	};
+app.controller('LandingController', function ($scope, AuthService, $state) {
+
+    function redirectToLibrary() {
+        var user = AuthService.getLoggedInUser();
+        if (user) $state.go('home');
+    }
+    redirectToLibrary();
+    $scope.toggleLogin = function () {
+        $scope.loggingIn = !$scope.loggingIn;
+        $scope.signingUp = false;
+    };
+    $scope.toggleSignup = function () {
+        $scope.signingUp = !$scope.signingUp;
+        $scope.loggingIn = false;
+    };
+
 });
